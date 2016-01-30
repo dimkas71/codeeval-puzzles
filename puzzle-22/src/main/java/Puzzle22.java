4afd379f3a3fbeaa19ee06a1edb41d6d75fbb08b
Trifanenko Dmitrij <dimkas71g@github.com>
2016-01-30 13:46:05 +0200

    add solution for the problem from site: https://www.codeeval.com/browse/22 add class Puzzle22 (with loop approach(memoized in an array of long)) add tests in spec Puzzle22Spec

--- puzzle-22/src/main/java/Puzzle22.java
+++ puzzle-22/src/main/java/Puzzle22.java
@@ -3,6 +3,14 @@ public class Puzzle22 {
         if (aNumber == 0) return 0;
         if (aNumber == 1) return 1;
 
-        return fib(aNumber - 2) + fib(aNumber - 1);
+        long[] fibs = new long[(int) aNumber + 1];
+        fibs[0] = 0;
+        fibs[1] = 1;
+
+        for (int i = 2; i <= aNumber; i++) {
+            fibs[i] = fibs[i - 2] + fibs[i - 1];
+        }
+
+        return fibs[(int) aNumber];
     }
 }
